d19554e9dc888c0f2a86af5f1dd7047f71f0181a
SiHoon61 <dlatlgns000@gmail.com>
2024-09-08 23:38:29 +0900

    init setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react', //@jsxImportSource @emotion/react를 생략해줌
+    }),
+    tsconfigPaths(), //경로 매핑 플러그인
+  ],
+});
